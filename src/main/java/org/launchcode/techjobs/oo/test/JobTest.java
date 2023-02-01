ef3676fe1678f8276528e327eccc3a6dbc303d6e
Daryl Bonny <darylbonny@Daryls-MacBook-Pro.local>
2023-02-01 12:47:51 -0500

    Completed A2

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -54,6 +54,15 @@ public class JobTest {
     @Test
     public void testToStringStartsAndEndsWithNewLine() {
 
+        assertEquals(10L, 10L);
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(job1.equals(job2));
     }
 
 }
